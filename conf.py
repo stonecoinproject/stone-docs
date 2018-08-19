b4c3f6e5ef58ba7d82eb87e04cc593946a72dd78
root <root@stonecoin.rocks>
2018-08-19 02:39:22 +0000

    added new theme

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import guzzle_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -76,12 +76,22 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Project Name",
+}
 # html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
